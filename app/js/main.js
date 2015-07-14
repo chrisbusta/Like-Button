d635d883ae9a6d2d43dcb983c7c6f02a6d9fee51
chris bustamante <chrisbusta@hotmsil.com>
2015-07-14 14:05:35 -0400

    changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,48 @@
-
-;(function (){
+(function(){
 
   'use strict';
 
-  angular.module('module', [])
+  var myApp = angular.module('myApp', []);
 
-it('should check ng-click', function() {
-  expect(element(by.binding('count')).getText()).toMatch('0');
-  element(by.css('button')).click();
-  expect(element(by.binding('count')).getText()).toMatch('1');
-});
+  myApp.controller('Ctrl', ['$scope', function($scope) {
+    $scope.authorFirst = 'Chris';
+    $scope.authorLast = 'Bustamante';
+    $scope.picture = 'https://pbs.twimg.com/profile_images/567733573709357057/fjW08y8z.jpeg';
+    $scope.likeCount = 0;
+    $scope.likeString = 'Likes';
 
-});
+    $scope.likeClick = function () {
+        if ($scope.likeCount === 0) {
+            $scope.likeString = 'Like';
+            $scope.likeCount += 1;
+        } else {
+            $scope.likeString = 'Likes';
+            $scope.likeCount += 1;
+        }
+    };
+
+  }]);
+
+}());
+
+
+
+// function Ctrl($scope) {
+//         }
+//     };
+
+// }
+// ;(function (){
+
+//   'use strict';
+
+//   angular.module('module', [])
+
+// it('should check ng-click', function() {
+//   expect(element(by.binding('count')).getText()).toMatch('0');
+//   element(by.css('button')).click();
+//   expect(element(by.binding('count')).getText()).toMatch('1');
+// });
+
+// });
 
